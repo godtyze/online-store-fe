96a2b5fda7630e93e530ac5d356a0f0cd75716a5
godtyze <omgfail301@gmail.com>
2023-03-27 22:37:03 +0600

    feat: add alias for path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/online-store-fe/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  }
 })
