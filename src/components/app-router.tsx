e1a9b7af2581507508d5a4408b91ed52e37205c6
godtyze <omgfail301@gmail.com>
2023-05-05 17:44:29 +0600

    refactor: migrate to 6.4+ version router config

--- src/components/app-router.tsx
+++ src/components/app-router.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
+import {FC} from 'react';
+import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
 import AppLayout from '@/components/app-layout';
 import Main from '@/pages/main';
 import Device from '@/pages/device';
@@ -10,20 +10,55 @@ import RequireAuth from '@/components/require-auth';
 import Cart from '@/pages/cart';
 import {CLIENT_ROUTES} from '@/config';
 
-const AppRouter: React.FC = () => {
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path={CLIENT_ROUTES.main} element={<AppLayout />}>
-      <Route index element={<Main />} />
-      <Route path={CLIENT_ROUTES.device} element={<Device />} />
-      <Route path={CLIENT_ROUTES.login} element={<Auth />} />
-      <Route path={CLIENT_ROUTES.registration} element={<Auth />} />
-      <Route element={<RequireAuth />}>
-        <Route path={CLIENT_ROUTES.profile} element={<Profile />} />
-        <Route path={CLIENT_ROUTES.cart} element={<Cart />} />
-      </Route>
-      <Route path={CLIENT_ROUTES.notFound} element={<NotFound />} />
-    </Route>
-  ), {basename: '/online-store-fe/'});
+const AppRouter: FC = () => {
+  const routerConfig = [
+    {
+      path: CLIENT_ROUTES.main,
+      element: <AppLayout />,
+      children: [
+        {
+          index: true,
+          element: <Main />
+        },
+        {
+          path: CLIENT_ROUTES.device,
+          element: <Device />
+        },
+        {
+          path: CLIENT_ROUTES.login,
+          element: <Auth />
+        },
+        {
+          path: CLIENT_ROUTES.registration,
+          element: <Auth />
+        },
+        {
+          element: <RequireAuth />,
+          children: [
+            {
+              path: CLIENT_ROUTES.profile,
+              element: <Profile />
+            },
+            {
+              path: CLIENT_ROUTES.cart,
+              element: <Cart />
+            }
+          ]
+        },
+        {
+          path: '*',
+          element: <Navigate to={CLIENT_ROUTES.notFound} />,
+        },
+        {
+          path: CLIENT_ROUTES.notFound,
+          element: <NotFound />,
+        },
+      ]
+    }
+  ];
+  const router = createBrowserRouter(routerConfig, {
+    basename: '/online-store-fe/'
+  });
 
   return (
     <RouterProvider router={router} />
